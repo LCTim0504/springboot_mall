e99b0dd4f4eff1879ecbf0f5d6df7232e93b8e30
Tim <tmsacc0504@gmail.com>
2022-12-19 22:33:05 +0800

    商品功能 - 刪除商品

--- springboot_mall/src/main/java/com/timlin/springboot_mall/dao/impl/ProductDao_Implement.java
+++ springboot_mall/src/main/java/com/timlin/springboot_mall/dao/impl/ProductDao_Implement.java
@@ -105,4 +105,14 @@ public class ProductDao_Implement implements ProductDao {
         namedParameterJdbcTemplate.update(sql, map);
     }
 
+    @Override
+    public void deleteProductById(Integer productId) {
+
+        String sql = "delete from product where product_id = :productId";
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("productId", productId);
+
+        namedParameterJdbcTemplate.update(sql, map);
+    }
 }
